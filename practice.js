3a487b845dd2d4549338f149fc2baf77841a1e04
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-01 15:56:44 +0600

    first commit

---
+++ practice.js
@@ -0,0 +1,22 @@
+// You went to the supermarket to buy some oranges and apples. Calculate how much money the shopkeeper will return.
+
+// Input:
+// The first line of the input is the taka you have. The second line is the cost of 1 kg of oranges and 1 kg of apples.
+
+// Output:
+// Print the result.
+
+// Sample Input:
+// 1000
+// 700
+
+// Sample Output:
+// 300
+var totalMoney = 1000;
+var orangePrice = 350;
+var applePrice =350;
+var totalPrice = (orangePrice + applePrice)
+console.log(totalPrice)
+
+var shopkeeperReturn = (totalMoney - totalPrice)
+console.log(shopkeeperReturn)
